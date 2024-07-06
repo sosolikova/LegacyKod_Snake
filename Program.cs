64c76e765bc8361b3d421ae6e710d85ecce2bc8b
Veronika Sosolikova <v_sosolikova@utb.cz>
2024-07-06 17:08:40 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegacyKod_Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
