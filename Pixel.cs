c55e42583f5c1bbc6e8141fc9d67d90189063e79
Veronika Sosolikova <v_sosolikova@utb.cz>
2024-07-07 01:35:03 +0200

    Remove class Pixel to new class file

---
+++ Pixel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Snake
+{ 
+    public class Pixel
+    {
+        public Pixel(int xPos, int yPos, ConsoleColor color)
+        {
+            XPos = xPos;
+            YPos = yPos;
+            ScreenColor = color;
+        }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public ConsoleColor ScreenColor { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -154,19 +154,6 @@ namespace Snake
             }
             return movement;
         }
-
-        class Pixel
-        {
-            public Pixel (int xPos, int yPos, ConsoleColor color)
-            {
-                 XPos = xPos;
-                 YPos = yPos;
-                 ScreenColor = color;
-            }
-            public int XPos { get; set; }
-            public int YPos { get; set; }
-            public ConsoleColor ScreenColor { get; set; }
-        }
     }  
 }
 
