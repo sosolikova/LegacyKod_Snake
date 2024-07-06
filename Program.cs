6cd595beb47f2e4b1313afbb0a7bb3843aba44ee
Veronika Sosolikova <v_sosolikova@utb.cz>
2024-07-07 00:45:19 +0200

    Class Pixel changed to Struct Pixel

--- Program.cs
+++ Program.cs
@@ -163,21 +163,20 @@ namespace Snake
 
             return movement;
         }
-    }
-
-    class Pixel
-    {
-        public Pixel (int xPos, int yPos, ConsoleColor color)
+        struct Pixel
         {
-            XPos = xPos;
-            YPos = yPos;
-            ScreenColor = color;
-        }
+            public Pixel (int xPos, int yPos, ConsoleColor color)
+            {
+                 XPos = xPos;
+                 YPos = yPos;
+                 ScreenColor = color;
+            }
 
-        public int XPos { get; set; }
-        public int YPos { get; set; }
-        public ConsoleColor ScreenColor { get; set; }
-       
+            public int XPos { get; set; }
+            public int YPos { get; set; }
+            public ConsoleColor ScreenColor { get; set; }
+        }
     }
+   
 }
 //¦
